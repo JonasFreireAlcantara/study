a1e83caf857d03df1608bd96c925dfd58ec129d3
Jonas Freire <jonasfreireperson@gmail.com>
2019-09-12 20:45:48 -0300

    controller for product model

---
+++ node_js/rocket_seat/node/initialize_db.sh
@@ -0,0 +1,6 @@
+
+# Pull the mongo image
+sudo docker pull mongo
+
+# Initializing the mongo image
+sudo docker start mongodb
---
+++ node_js/rocket_seat/node/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Initializing app
+const app = express();
+
+// MongoDB connection with mongoose
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true
+});
+require('./src/models/Product');
+
+// Routes
+app.use('/api', require('./src/routes'));
+
+// Startup app to listen at port 3001
+app.listen(3001);
