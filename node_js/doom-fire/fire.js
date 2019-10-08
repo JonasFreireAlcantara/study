e88caf1cf1b506d8ab9890b00ddb4ad993824680
Jonas Freire <jonasfreireperson@gmail.com>
2019-10-08 11:16:21 -0300

    addition of random wind, was AMAZING !

--- node_js/doom-fire/fire.js
+++ node_js/doom-fire/fire.js
@@ -1,7 +1,7 @@
 const firePixelsArray = [];
-let fireWidth = 60;
+let fireWidth = 120;
 let fireHeight = 40;
-let sentido = 2;
+let sentido = 0;
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 
 
@@ -9,7 +9,7 @@ function start() {
     createFireDataStructure();
     createFireSource();
 
-    setInterval(calculateFirePropagation, 100);
+    setInterval(calculateFirePropagation, 1);
 }
 
 function changeWindDirection(value) {
@@ -37,7 +37,7 @@ function calculateFirePropagation() {
 }
 
 function renderFire() {
-    let html = '<table >'
+    let html = '<table cellpadding=0 cellspacing=0 >'
 
     for (let row = 0; row < fireHeight; row++) {
         html += '<tr>'
@@ -67,39 +67,49 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
         return;
     }
 
-    const decay = Math.floor(Math.random() * 3);
+    const decay = Math.ceil(Math.random() * 1);
+    const windDirection = Math.floor(Math.random() * 3) - 1;
     const belowPixelFireIntensity = firePixelsArray[belowPixelIndex];
     let newFireIntensity = (belowPixelFireIntensity - decay) >= 0 ? (belowPixelFireIntensity - decay) : 0;
 
-    firePixelsArray[currentPixelIndex] = newFireIntensity;
+    firePixelsArray[currentPixelIndex + windDirection] = newFireIntensity; // left
+}
 
-    switch(sentido) {
-        case 0:
-            firePixelsArray[currentPixelIndex - decay] = newFireIntensity; // left
-            break;
-        case 1:
-            firePixelsArray[currentPixelIndex] = newFireIntensity; // no wind
-            break;
-        case 2:
-            firePixelsArray[currentPixelIndex + decay] = newFireIntensity; // right
-            break;
+function setFireSource(value) {
+    const overflowPixelIndex = fireWidth * fireHeight;
+    for (let column = 0; column <= fireWidth; column++) {
+        const pixelIndex = (overflowPixelIndex - fireWidth) + column;
+        firePixelsArray[pixelIndex] = value;
     }
 }
 
 function createFireSource() {
-    const overflowPixelIndex = fireWidth * fireHeight;
-    for (let column = 0; column <= fireWidth; column++) {
-        const pixelIndex = (overflowPixelIndex - fireWidth) + column;
-        firePixelsArray[pixelIndex] = 36;
-    }
+    setFireSource(36);
 }
 
 function increaseFireSource() {
     const overflowPixelIndex = fireWidth * fireHeight;
 
     for (let column = 0; column < fireWidth; column++) {
+        const increase = Math.floor(Math.random() * 14);
         const pixelIndex = (overflowPixelIndex - fireWidth) + column;
-        const currentFireIntensity = firePixelsArray
+        const currentFireIntensity = firePixelsArray[pixelIndex];
+        const newFireIntensity = (currentFireIntensity + increase) > 36 ? 36 : (currentFireIntensity + increase);
+        
+        firePixelsArray[pixelIndex] = newFireIntensity;
+    }
+}
+
+function decreaseFireSource() {
+    const overflowPixelIndex = fireWidth * fireHeight;
+
+    for (let column = 0; column < fireWidth; column++) {
+        const decay = Math.floor(Math.random() * 14);
+        const pixelIndex = (overflowPixelIndex - fireWidth) + column;
+        const currentFireIntensity = firePixelsArray[pixelIndex];
+        const newFireIntensity = (currentFireIntensity - decay) < 0 ? 0 : (currentFireIntensity - decay);
+
+        firePixelsArray[pixelIndex] = newFireIntensity;
     }
 }
 
